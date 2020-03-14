0604ebbea6918108960c878f893c3fdee89d057c
dit19048@uop.gr <dit19048@uop.gr>
2020-03-14 20:58:42 +0200

    lab1 final program

--- lab1,1/Source.asm
+++ lab1,1/Source.asm
@@ -1,16 +1,24 @@
-title lab:1 Evaluating Simple Arithmetic Expressions
-include Irvine32.inc 
-
+title Laboratory 1 : Evaluating Simple Arithmetic Expressions
+Include Irvine32.inc
+	p EQU 8d
 .data
-
-
-
-
-
+	x SDWORD 16d
+	y SDWORD 154d
+	z SDWORD -17d
+	w SDWORD ?
 .code
- main proc
-
-
-   exit 
- main endp
- end main 
+main PROC
+	; w = (x + y) - (50 + x + y + z - p)
+	MOV eax,x	; EAX = x
+	ADD eax,y   ; EAX = x+y
+	
+	MOV ebx, 50d	;EBX=50
+	ADD ebx, eax    ;EBX=50+x+y
+	ADD ebx, z		;EBX=50+x+y+z
+	SUB ebx, p		;EBX=50+x+y+z-p
+	SUB eax,ebx		;EAX=(x + y) - (50 + x + y + z - p);
+	MOV w,eax		;w=(x + y) - (50 + x + y + z - p);
+	call WriteInt
+		exit
+main ENDP
+END main
